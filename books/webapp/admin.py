0f49b404429d7ccc78cd1aadda4ab79bd9b02355
Kurbanaliev <kurbanalieverlan@gmail.com>
2021-03-06 13:15:49 +0600

    Реализовал модели и админку!

--- books/webapp/admin.py
+++ books/webapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from webapp.models import Guest
+
+
 # Register your models here.
+
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'mail', 'date_creat', 'date_update']
+    list_filter = ['date_creat']
+    search_fields = ['mail', 'date_creat']
+    fields = ['id', 'name', 'mail', 'description']
+    readonly_fields = ['id', 'date_update']
+
+admin.site.register(Guest, GuestAdmin)
--- books/webapp/models.py
+++ books/webapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Guest(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    mail = models.EmailField(max_length=100)
+    description = models.TextField(max_length=2000, null=False, blank=False)
+    date_creat = models.DateField(auto_now_add=True, editable=False, blank=True)
+    date_update = models.DateTimeField(auto_now=True, editable=True, blank=True)
+
+    class Meta:
+        db_table = 'Books'
+        verbose_name = 'Гость'
+        verbose_name_plural = 'Гости'
+
+    def __str__(self):
+        return f'{self.id}. {self.date_creat}: {self.name}'
